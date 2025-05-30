44f942c359f08f75c71ca7501b3d99fa26a338fd
LucasLira200709 <lucas68289401@gmail.com>
2025-05-30 13:55:01 -0300

    Add files via upload

---
+++ landPage/digianscript.js
@@ -0,0 +1,17 @@
+function irparasobre() {
+    const imagem = document.getElementById('isobre');
+    const posicaoY = imagem.getBoundingClientRect().top;
+    window.scrollTo( {
+        top: posicaoY,
+        behavior: 'smooth'
+    });
+}
+function irparaServ() {
+    const imagem = document.getElementById('isobre');
+    const posicaoY = imagem.getBoundingClientRect().top;
+    window.scrollTo( {
+        top: posicaoY,
+        behavior: 'smooth'
+    });
+    
+}
