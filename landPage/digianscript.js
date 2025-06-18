6d36a70618739c7afb723328b665dd001fe4a6ca
LucasLira200709 <lucas68289401@gmail.com>
2025-06-18 13:48:54 -0300

    Update digianscript.js

--- landPage/digianscript.js
+++ landPage/digianscript.js
@@ -1,14 +1,37 @@
 function irparasobre() {
     const imagem = document.getElementById('isobre');
     const posicaoY = imagem.getBoundingClientRect().top;
+
     window.scrollTo( {
         top: posicaoY,
         behavior: 'smooth'
     });
+    
 }
 function irparaServ() {
-    const imagem = document.getElementById('isobre');
+    const imagem = document.getElementById('iserv');
     const posicaoY = imagem.getBoundingClientRect().top;
+
+    window.scrollTo( {
+        top: posicaoY,
+        behavior: 'smooth'
+    });
+    
+}
+function irparaContato() {
+    const imagem = document.getElementById('icontato');
+    const posicaoY = imagem.getBoundingClientRect().top;
+
+    window.scrollTo( {
+        top: posicaoY,
+        behavior: 'smooth'
+    });
+    
+}
+function irparaInicio() {
+    const imagem = document.getElementById('inicio');
+    const posicaoY = imagem.getBoundingClientRect().top;
+
     window.scrollTo( {
         top: posicaoY,
         behavior: 'smooth'
